bc44e57fdf17d3c699431073ecfb24c172f88b4d
Bianca <biancancanal@gmail.com>
2021-07-27 09:32:01 -0500

    commit tree 859ae17524031737cc1debf0452dd942513ff0d5

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr.Chocolate', 12, 'M', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 15, 'M', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 13, 'F', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 8, 'M', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 6, 'F', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 18, 'M', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 2, 'F', 'Brown', 'Nice', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 20,  'M', 'Brown', 'Mean', 1);
